82ea51c82941dd14e7dd2dbb2cffa73eba05ba6f
RODO.L <wuaicot8@gmail.com>
2023-05-06 10:20:35 -0400

    eliminado bug del componente

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -5,8 +5,7 @@ import style from './Loading.module.css';
 const Loading = () => {
     return(
         <div  className={style.container}>
-            <img src= 'loading.gif' ></img>
-    
+            <img src= 'loading.gif' alt="CARGANDO" ></img>    
         </div>
     )
 }
