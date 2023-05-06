d457c9193b11f6ff4fc018f393b679cc8f6a973b
RODO.L <wuaicot8@gmail.com>
2023-05-06 14:28:48 -0400

    eliminando bugs

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,44 +1,72 @@
-import React, { useEffect } from "react";
-import style from './Pagination.module.css'
+import React, { useEffect, useCallback } from "react";
+import style from "./Pagination.module.css";
 
-export default function Pagination({pagination, dogsPerPage, dogsLength, currentPage}){ 
+export default function Pagination({
+  pagination,
+  dogsPerPage,
+  dogsLength,
+  currentPage,
+}) {
+  let pageQnty = Math.ceil(dogsLength / dogsPerPage);
+  const pageNumbers = [];
 
-    let pageQnty = Math.ceil(dogsLength / dogsPerPage);  
-    const pageNumbers = [];
+  for (let i = 1; i <= pageQnty; i++) {
+    pageNumbers.push(i);
+  }
 
-    for (let i = 1; i <= pageQnty ; i++){ 
-        pageNumbers.push(i)
-    }
- 
-    function prevPage(page){ // <
-        if(page!==1) pagination(page-1) 
-    }
+  const prevPage = useCallback(
+    (page) => {
+      if (page !== 1) pagination(page - 1);
+    },
+    [pagination]
+  );
 
-    function nextPage(page){ // > 
-        if(page!== pageQnty) pagination(page+1)
-    }
-    
-    
-    useEffect(()=> {window.addEventListener("keydown",handleKey); 
-    return () => window.removeEventListener("keydown",handleKey)},[pagination]) 
+  const nextPage = useCallback(
+    (page) => {
+      if (page !== pageQnty) pagination(page + 1);
+    },
+    [pagination, pageQnty]
+  );
 
-   function handleKey(event){
-        if (event.keyCode === 37) prevPage(currentPage); 
-        if (event.keyCode === 39) nextPage(currentPage);
-    }
+  useEffect(() => {
+    const handleKey = (event) => {
+      if (event.keyCode === 37) prevPage(currentPage);
+      if (event.keyCode === 39) nextPage(currentPage);
+    };
 
+    window.addEventListener("keydown", handleKey);
+    return () => window.removeEventListener("keydown", handleKey);
+  }, [currentPage, nextPage, prevPage]);
 
-    return (
-        <nav>
-            <div className={style.pagination}>
-                <span className={style.page_number}><a onClick={ () => pagination(1)}>{'<<'}</a></span>
-                <span className={style.page_number}><a onClick={ () => prevPage(currentPage)}>{'<'}</a></span>
-                {pageNumbers &&pageNumbers.map( page =>
-                    <span className={page!==currentPage? style.page_number : style.page_number_current} key={page}><a onClick={ () => pagination(page)}>{page}</a></span>
-                )}
-                <span className={style.page_number}><a onClick={ () => nextPage(currentPage)}>{'>'}</a></span>
-                <span className={style.page_number}><a onClick={ () => pagination(pageQnty)}>{'>>'}</a></span>
-            </div>  
-        </nav>
-    )
+  return (
+    <nav>
+      <div className={style.pagination}>
+        <span className={style.page_number}>
+          <button onClick={() => pagination(1)}>{"<<"}</button>
+        </span>
+        <span className={style.page_number}>
+          <button onClick={() => prevPage(currentPage)}>{"<"}</button>
+        </span>
+        {pageNumbers &&
+          pageNumbers.map((page) => (
+            <span
+              className={
+                page !== currentPage
+                  ? style.page_number
+                  : style.page_number_current
+              }
+              key={page}
+            >
+              <button onClick={() => pagination(page)}>{page}</button>
+            </span>
+          ))}
+        <span className={style.page_number}>
+          <button onClick={() => nextPage(currentPage)}>{">"}</button>
+        </span>
+        <span className={style.page_number}>
+          <button onClick={() => pagination(pageQnty)}>{">>"}</button>
+        </span>
+      </div>
+    </nav>
+  );
 }
