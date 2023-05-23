a8418a94064bee3776c5e09fb436b7978c6b433a
RODO.L <wuaicot8@gmail.com>
2023-05-23 14:46:11 -0400

    deployando el back

--- api/src/db.js
+++ api/src/db.js
@@ -6,14 +6,21 @@ const fs = require('fs');
 
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
 //ORM- creamos instancia de sequelize | cadena de conexión
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+
+const sequelize = new Sequelize(
+  DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
 //para filtrar los archivos que contienen los modelos de Sequelize y requerirlos dinámicamente.
 const basename = path.basename(__filename);
 
