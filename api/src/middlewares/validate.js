4af7361c3a2e4b697a906f86d82f70332df26ae9
RODO.L <wuaicot8@gmail.com>
2023-08-09 15:07:16 -0400

    extracting api directory

--- api/src/middlewares/validate.js
+++
@@ -1,8 +0,0 @@
-// evaluamos si estan presentes los parametros necesarios
-const validate = (req, res, next) =>{
-    const {name,height_min, height_max, weight_min, weight_max,life_span} = req.body;
-    if (!name || (!height_min && !height_max) || (!weight_min && !weight_max) || !life_span ) return res.status(400).json({ error: 'Faltan campos obligatorios'});
-    next(); 
-  }
-
-  module.exports = {validate}
