edec1c7431832fcad8e2437d112f7a3bdd14bf87
RODO.L <wuaicot8@gmail.com>
2023-08-13 16:53:03 -0400

    moving .json

--- api/src/db.js
+++ api/src/db.js
@@ -55,3 +55,6 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+
+
+
