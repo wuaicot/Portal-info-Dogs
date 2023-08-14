6e13bbfeddc950ce8771c6e0004fc83853b057fa
RODO.L <wuaicot8@gmail.com>
2023-08-14 19:28:28 -0400

    ssl off

--- api/src/db.js
+++ api/src/db.js
@@ -15,7 +15,7 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
   // dialect: 'postgres',
   // protocol: 'postgres',
   // dialectOptions: {
-  //   ssl: { require: false },
+  //   ssl: { require: true },
   // },
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
